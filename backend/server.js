3018a3d738a3b01da474cc21271f45dfcbc558be
Pridhvinath Chowdary <mpnc2296@gmail.com>
2023-09-18 06:35:53 -0400

    Server Setup

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const app = require("./app");
+
+
+
+process.on("uncaughtException", (err) => {
+    console.log(`Error: $(err.message)`);
+    console.log("Shutting down the server for uncaught Exception");
+})
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({
+        path: "backend/config/.env"
+    });
+}
+
+
+const server = app.listen(process.env.PORT,() =>{
+    console.log(`Server is running on http://localhost:${process.env.PORT}`)
+})
+
+
+process.on("unhandledRejection", (err) => {
+    console.log(`shutting down the server for $(err.message)`);
+    console.log(`shutting down the server for unhandle promise rejection`);
+
+    server.close(() => {
+        process.exit(1);
+    });
+})
