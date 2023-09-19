84102708debbdec94311d66164ae4720edc56995
Pridhvinath Chowdary <mpnc2296@gmail.com>
2023-09-19 05:15:34 -0400

    Async errors

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const Errorhandler = require("./utils/ErrorHandler");
 const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+
+app.use(express.json);
+app.use(cookieParser);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(fileUpload({useTempFiles: true}));
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({
@@ -8,5 +17,6 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 
+app.use(Errorhandler);
 module.exports = app;
 
