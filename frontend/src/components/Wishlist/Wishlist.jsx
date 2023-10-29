b00572330efebf2dfbdbff8062d844bb4edffd77
prudhvi2297 <mpnc2296@gmail.com>
2023-10-29 06:21:21 -0400

    Added Whishlist

--- frontend/src/components/Wishlist/Wishlist.jsx
+++ frontend/src/components/Wishlist/Wishlist.jsx
@@ -1,51 +1,51 @@
 import React, { useState } from "react";
-import { RxCross1 } from "react-icons/rx";
+import { IoCloseCircle, IoHeart } from "react-icons/io5";
 import styles from "../../styles/styles";
-import { IoBagHandleOutline } from "react-icons/io5";
-import {BsCartPlus} from "react-icons/bs";
-import { Link } from "react-router-dom";
-import { AiOutlineHeart } from "react-icons/ai";
+import { BsTrash, BsCartPlus } from "react-icons/bs";
 
 const Wishlist = ({ setOpenWishlist }) => {
   const cartData = [
     {
-      name: "Iphone 14 Pro  max",
-      description: "test",
+      name: "Iphone 14 Pro Max 1TB 8GB RAM A16 Bionic",
+      description: "Test",
       price: 999,
     },
     {
-      name: "Iphone 14 Pro  max",
-      description: "test",
+      name: "Iphone 14 Pro Max",
+      description: "Test",
       price: 2999,
     },
     {
-      name: "Iphone 14 Pro  max",
-      description: "test",
+      name: "Iphone 14 Pro Max",
+      description: "Test",
       price: 3999,
     },
   ];
 
   return (
     <div className="fixed top-0 left-0 w-full bg-[#0000002d] h-screen z-10">
-      <div className="fixed top-0 right-0 min-h-full w-[25%] bg-white flex flex-col justify-between shadow-sm">
+      <div className="fixed top-0 right-0 min-h-full w-[25%] bg-white flex flex-col justify-between shadow-lg rounded-l-3xl">
         <div>
           <div className="flex w-full justify-end pt-5 pr-5">
-            <RxCross1
-              size={20}
-              className="cursor-pointer"
+            <IoCloseCircle
+              size={30}
+              className="cursor-pointer text-gray-400 hover:text-gray-600"
               onClick={() => setOpenWishlist(false)}
             />
           </div>
 
-          <div className={`${styles.noramlFlex} p-4`}>
-            <AiOutlineHeart size={25} />
-            <h5 className="pl-2 text-[20px] font-[500]">3 items</h5>
+          <div className={`${styles.normalFlex} p-4 items-center`}>
+            <IoHeart size={25} className="text-[#D02222]" />
+            <h5 className="pl-2 text-2xl font-semibold">
+              {cartData.length} items
+            </h5>
           </div>
 
           <br />
-          <div className="w-full border-t">
+          <div className="w-full border-t" style={{ maxHeight: "70vh", overflowY: "auto" }}>
             {cartData &&
-              cartData.map((i, index) => <CartSingle key={index} data={i} />)}
+              cartData.map((item, index) => <CartSingle key={index} data={item} />)
+            }
           </div>
         </div>
       </div>
@@ -54,29 +54,41 @@ const Wishlist = ({ setOpenWishlist }) => {
 };
 
 const CartSingle = ({ data }) => {
-  const [value, setValue] = useState(1);
-  const totalPrice = data.price * value;
+  const [quantity, setQuantity] = useState(1);
+  const totalPrice = data.price * quantity;
 
   return (
     <div className="border-b p-4">
-      <div className="w-full flex items-center">
-        <RxCross1 className="cursor-pointer" />
-        <img
-          src="https://www.notebookcheck.net/uploads/tx_nbc2/AppleiPhone14Pro__1__01.JPG"
-          alt=""
-          className="w-[80px] h-[80px] ml-2"
-        />
-        <div className="pl-[5px]">
-          <h1>{data.name}</h1>
-          <h4 className="font-[600] text-[17px] pt-[3px] text-[#d02222] font-Roboto">
+      <div className="w-full flex items-center justify-between">
+        <div className="flex items-center">
+          <img
+            src="https://www.notebookcheck.net/uploads/tx_nbc2/AppleiPhone14Pro__1__01.JPG"
+            alt=""
+            className="w-[80px] h-[80px] object-cover rounded"
+          />
+          <div className="pl-[10px]">
+            <h1 className="text-lg font-semibold">{data.name}</h1>
+            <h4 className="text-base text-gray-600">
+              US$ {data.price} * {quantity}
+            </h4>
+            <h4 className="text-2xl text-[#D02222] font-medium pt-1">
               US$ {totalPrice}
-          </h4>
+            </h4>
+          </div>
         </div>
-      <div>
-        <BsCartPlus size={25} className="cursor-pointer" title="Add to cart" />
-      </div>
+        <div className="flex items-center">
+          <BsTrash
+            size={24}
+            className="cursor-pointer text-gray-400 hover:text-red-600 mr-3"
+          />
+          <BsCartPlus
+            size={24}
+            className="cursor-pointer text-teal-500 hover:text-teal-600"
+          />
+        </div>
       </div>
     </div>
   );
 };
+
 export default Wishlist;
