53acd929685904ce96f58fd58fe45a7e1999f2a1
prudhvi2297 <mpnc2296@gmail.com>
2023-10-29 06:27:41 -0400

    Added Seller Activation Page

---
+++ frontend/src/pages/SellerActivationPage.jsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { server } from "../server";
+
+const SellerActivationPage = () => {
+  const { activation_token } = useParams();
+  const [error, setError] = useState(false);
+  const navigate = useNavigate(); // Import useNavigate from react-router-dom
+
+  useEffect(() => {
+    if (activation_token) {
+      const sendRequest = async () => {
+        try {
+          const response = await axios.post(`${server}/shop/activation`, {
+            activation_token,
+          });
+          console.log(response.data);
+
+          // Add a delay of 5 seconds (5000 milliseconds) before redirecting to the login page
+          setTimeout(() => {
+            navigate("/"); // Redirect to the Home page
+          }, 5000);
+        } catch (err) {
+          setError(true);
+        }
+      };
+      sendRequest();
+    }
+  }, [activation_token, navigate]); // Include activation_token and navigate in the dependency array
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-50">
+      <div className="bg-white shadow-md rounded-lg p-6 max-w-sm w-full">
+        {error ? (
+          <p className="text-red-500 text-center font-semibold text-lg">
+            Your token is expired!
+          </p>
+        ) : (
+          <>
+            <p className="text-green-500 text-center font-semibold text-lg mb-4">
+              Your account has been created successfully!
+            </p>
+            <div className="text-center">
+              <img
+                src={`${process.env.PUBLIC_URL}/success.gif`}
+                alt="Success Notification"
+                className="w-50 h-30 mx-auto"
+              />
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SellerActivationPage;
